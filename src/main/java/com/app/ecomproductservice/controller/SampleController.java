b305a4d7c6b7b4098a486c6f7ffc1a896c44ad72
vinay <vinaypatel275@gmail.com>
2024-07-16 17:21:57 +0530

    First commit

---
+++ src/main/java/com/app/ecomproductservice/controller/SampleController.java
@@ -0,0 +1,26 @@
+package com.app.ecomproductservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/say")
+public class SampleController {
+
+    @GetMapping("/hello/{name}/{symbol}")
+    public String sayHello(@PathVariable("name") String name, @PathVariable("symbol") String S)
+    {
+        return "Hello" + " " + name + " "+ S;
+    }
+
+    @GetMapping("/hi/{name}/{times}")
+    public String sayHi(@PathVariable("name") String name,@PathVariable("times") int times) {
+        String output = "";
+        for (int i = 0; i < times; i++) {
+            output = output + "Hi" + " " + name;
+        }
+        return output;
+    }
+}
